4825f97cc38bc04a2cbeb1b158a318684f73173a
Puja Sah <pujasah01.1998@gmail.com>
2021-03-09 14:46:32 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -12,19 +12,80 @@ var chocolates = [
 ];
 
 //Progression 1: Add ___ chocolates of ____ color
+/* Create a function `addChocolates()` which takes `chocolates,color, count`
+  as arguments and adds them to the dispenser from the top.*/
 
+addChocolates = (chocolates, color, count)=>{
+    if (count <= 0){
+        return "Chocolate array is empty"; }
+    for (let i = 0; i <= count; i++)
+        chocolates.unshift(color);
+    return chocolates;
+}
 
 //Progression 2: Remove ___ chocolates from the top the dispenser
+/*Create a function removeChocolates() that takes number,chocolates as an argument and 
+returns an array of chocolates that have been removed from the top. */
 
+removeChocolates = (chocolates, number) => {
+    let chocolate = [];
+    if (number <= 0)
+        return "Dispenser cannot be empty";
+    if (number > chocolates.length)
+        return "Insufficient chocolates in the dispenser";
+    for (let i = 0; i < number; i++)
+        chocolate.push(chocolates.shift());
+    return chocolate;
+}
 
 //Progression 3: Dispense ___ chocolates
+/*create a function dispenseChocolates() that dispenses chocolate at her will. It takes
+number, chocolates as an argument and returns an array of chocolates from the bottom. */
 
+dispenseChocolates = (number,chocolates) =>{
+    let chocolate = [];
+    if (number <= 0)
+        return "It cannot be zero";
+    for (let i = 0; i < Number; i++)
+        chocolate.push(chocolates.push());
+    return chocolate;
+}
 
 //Progression 4: Dispense ___ chocolates of ____ color
+/*Create a function dispenseChocolatesOfColor() which takes chocolates,number, color as 
+arugments and return an array of chocolates from the bottom of the given color.*/
 
+dispenseChocolatesOfColor=(chocolates, number, color)=>{
+    if(number<=0)
+        return "Number cannot be zero/negative";
+    if(number>chocolates.length)
+        return "Insufficient chocolates in the dispenser";
+    let chocolate=chocolates.slice(chocolates.length-number, chocolates.length);
+    return chocolate;    
+}
 
-//Progression 5: Display ___ chocolates of each color. Return array of numbers [green, silver, blue, crimson, purple, red, pink]
-
+/* Progression 5: Display ___ chocolates of each color. Return array of numbers 
+[green, silver, blue, crimson, purple, red, pink] */
+noOfChocolates=(chocolates)=>{
+    let arr=[];
+    for(let i=0; i<chocolates.length; i++)
+        chocolates[i] = -1;
+   
+    for(let i=0; i<chocolates.length; i++){
+        let count = 1;
+        for(let j=i+1; j<chocolates.length; j++){
+            if(chocolates[i]==chocolates[j]){
+                count++;
+                chocolates[j] = 0;
+            }
+        }
+        if(chocolates[i] != 0){
+            chocolates[i] = count;
+        }
+    }
+    let output=chocolates.filter(element=> element !== 0);
+    return output;
+}
 
 //Progression 6: Sort chocolates based on count in each color. Return array of colors
 
